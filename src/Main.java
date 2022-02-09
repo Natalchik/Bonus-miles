467e8d40fadfb0b5e85bac767e24ce4f5ad0575e
Nataly Kalinina <annataly-1238@ya.ru>
2022-02-09 17:18:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 1000; // цена билета
+
+        int mile = 20; // количество рублей за милю
+
+        System.out.println("Начислено бонусных миль:");
+        System.out.println((price / mile));
+
+
+    }
+}
